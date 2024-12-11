b46b0f6b8b0e0fcf6aaa153dc41fbae90552e511
duruaydogdu <duruaydogdu02@gmail.com>
2024-12-11 23:04:40 +0300

    feat(works): add TasarimCalismasi and CalismaYapisi

---
+++ iOS-Bootcamp-KasımAdalan/works/CalismaYapisi/CalismaYapisi/AnasayfaVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalismaYapisi
+//
+//  Created by Duru Aydoğdu on 11.12.2024.
+//
+
+import UIKit
+
+class AnasayfaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOS-Bootcamp-KasımAdalan/works/TasarimCalismasi/TasarimCalismasi/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  TasarimCalismasi
+//
+//  Created by Duru Aydoğdu on 30.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.title = "Pizza"
+        
+        let appearance = UINavigationBarAppearance()
+        
+        appearance.backgroundColor = UIColor(named: "anaRenk")
+        appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "yaziRenk1")!, .font: UIFont(name: "Pacifico-Regular", size: 22)!] // array
+        navigationController?.navigationBar.barStyle = .black
+        
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+
+    }
+
+
+}
+
